e2299b54d0c26d0232033cebfae96dfb8ae7510b
Maciej Sikora <maciejsikora2302@gmail.com>
2020-03-15 16:15:40 +0100

    generating works

--- lab2/program.c
+++ lab2/program.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
  
+
+
+
 int main(int argc, char **argv) 
 {
+    srand(time(NULL));
+
     if(argc < 5){
         printf("Too few arguments :3\n");
         return 1;
@@ -16,7 +22,26 @@ int main(int argc, char **argv)
         int length_of_record = atoi(length_of_record_tmp); 
         printf("File name: %s\nRecords to create: %d\nLength: %d\n", file_name, number_of_records_to_create, length_of_record);
         //Do tąd jak na razie cycuś glancuś pizdeczka
-        //no
+        
+        FILE* file = fopen(file_name, "w");
+        for(int i = 0; i < number_of_records_to_create; i++){
+            char* mini_buffor = (char*) calloc(1, sizeof(char));
+            for(int j = 0; j < length_of_record; j++){
+                
+                int ran = rand()%26;
+                if(rand()%2 == 0){
+                    sprintf(mini_buffor, "%c", ran + 65); //Duże litery
+                }else{
+                    sprintf(mini_buffor, "%c", ran + 97); //Małe litery
+                }
+
+                fwrite(mini_buffor, 1, 1, file);
+            }
+            fwrite("\n", 1, 1, file);
+        }
+
+
+        fclose(file);
 
 
 
